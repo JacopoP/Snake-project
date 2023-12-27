0b4d1acefcdd349ff788806abad6f8801d6f7dfe
Jac22 <jacopo.prudentino@gmail.com>
2023-12-27 23:10:27 +0100

    Improved ui

--- script.js
+++ script.js
@@ -19,6 +19,17 @@ let snake = [];
 
 let head;
 
+// fruit info
+
+let fruit;
+
+// ui info
+
+const start_ui = document.getElementById('start_ui');
+const end_ui = document.getElementById('end_ui');
+const end_ui_result = document.querySelector('#end_ui h2');
+const end_ui_score = document.querySelector('#end_ui p');
+
 // engine
 
 //game strart
@@ -26,6 +37,7 @@ let head;
 let playing = false;
 
 document.addEventListener('click', function () {
+    start_ui.classList.add('hide');
     if (!playing) {
         for (let i = 0; i < 360; i++) {
             table[i].classList.remove('snakeBody', 'snakeHead', 'fruit');
@@ -37,19 +49,16 @@ document.addEventListener('click', function () {
         snake.forEach(snakeSquare => table[snakeSquare].classList.add('snakeBody'));
         table[head].classList.add('snakeHead');
         table[fruit].classList.add('fruit');
+        end_ui.classList.add('hide');
 
         // clock
         gameOn = setInterval(function () {
             movement();
-        }, 500);
+        }, 300);
 
         playing = true;
     }
 })
-
-// fruit info
-
-let fruit;
 
 // functions
 // movement function
@@ -124,6 +133,16 @@ document.addEventListener('keydown', function (event) {
 // game over
 
 function gameOver() {
+    if (snake.length == 360) {
+        end_ui_result.style.color = 'green'
+        end_ui_result.innerHTML = 'You win!'
+    }
+    else {
+        end_ui_result.style.color = 'red'
+        end_ui_result.innerHTML = 'Game Over';
+        end_ui_score.innerHTML = `You made ${snake.length - 3} points`;
+    }
+    end_ui.classList.remove('hide');
     clearInterval(gameOn);
     playing = false;
 }
